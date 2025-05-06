2d74bbd3672c189067b871ed14175a8cb3ce4b67
SaritaPereira <saritapg2010@hotmail.com>
2025-05-06 11:30:50 -0300

    Atividade 02 concluida

---
+++ src/Atv03.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Atv03() {
+  return (
+    <>
+    <h1> Atividade 3</h1>
+    
+    <br/>
+    <Link to="/"> Voltar</Link>
+    </>
+  );
+}
+
+export default Atv03;
---
+++ src/Atv04.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Atv04() {
+  return (
+    <>
+    <h1> Atividade 4</h1>
+    
+    <br/>
+    <Link to="/"> Voltar</Link>
+    </>
+  );
+}
+
+export default Atv04;
